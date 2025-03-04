fdfb34143d2e66c9971c9db07c71e475c0298f0e
Anesipho-Mazaza <aneshmazaza@gmail.com>
2025-03-04 13:59:12 +0200

    This function saves the user's name and updates the welcome message on the To-Do List

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
     loadUserName();
     displayTasks();
 });
+// Function to Save User's Name
+function saveUserName() {
+    let userName = document.getElementById("userNameInput").value.trim();
+    
+    if (userName !== "") {
+        localStorage.setItem("userName", userName);
+        document.getElementById("headerText").innerHTML = `${userName}, Welcome! Your To-Do List`;
+        document.getElementById("namePopup").style.display = "none";
+    }
+}
